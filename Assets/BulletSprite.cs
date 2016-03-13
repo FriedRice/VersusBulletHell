0b1558cf74dd3c3f3e7521aaa568104a7861d79a
Stali <sstalev@umich.edu>
2016-03-13 15:08:31 -0400

    billets

---
+++ Assets/BulletSprite.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletSprite : MonoBehaviour {
+    public Sprite[] DieAnim;
+    public Sprite[] NormalAnim;
+
+    public float animPlaySpeed = 0.1f;
+
+    bool alive = true;
+
+    SpriteRenderer sr;
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        StartCoroutine(anim());
+	}
+
+    IEnumerator anim()
+    {
+        while (alive)
+        {
+            for(int c = 0; c < NormalAnim.Length; ++c)
+            {
+                sr.sprite = NormalAnim[c];
+                yield return new WaitForSeconds(animPlaySpeed);
+            }
+        }
+    }
+
+    IEnumerator dieanim()
+    {
+
+        for (int c = 0; c < DieAnim.Length; ++c)
+        {
+            sr.sprite = DieAnim[c];
+            yield return new WaitForSeconds(animPlaySpeed);
+        }
+    }
+
+    public void Dissipate()
+    {
+        alive = false;
+        StartCoroutine(dieanim());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
