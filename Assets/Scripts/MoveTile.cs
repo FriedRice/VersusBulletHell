05a64fc3d9b470d3ea409d365c55140cde442f9d
alanjara <alanjara@umich.edu>
2016-03-14 19:31:48 -0400

    Moving refractions

---
+++ Assets/Scripts/MoveTile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class MoveTile : MonoBehaviour {
+
+    public float speed = 1;
+    public float end = -10;
+    public float start = 10;
+  public  List<GameObject> tiles;
+    // Use this for initialization
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        foreach (GameObject tile in tiles)
+        {
+            Vector3 temp = tile.transform.position;
+            temp.y -= speed;
+            if (temp.y < end)
+                temp.y += start - end;
+            tile.transform.position = temp;
+        }
+    }
+}
