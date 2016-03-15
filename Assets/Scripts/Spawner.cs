d995b9b1b63b573b3c17bb0c6c1962155a260faa
alanjara <alanjara@umich.edu>
2016-03-14 21:17:51 -0400

    increasing rate

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,7 +7,7 @@ public class Spawner : MonoBehaviour {
     public float delay = 10;
     public float timer = 0;
     void Start() {
-
+        timer = delay - 3;
     }
 
     // Update is called once per frame
@@ -21,6 +21,8 @@ public class Spawner : MonoBehaviour {
             Vector2 temp = new Vector2(x, y);
             enemy.transform.Find("RightFish").gameObject.GetComponent<Enemy>().Initialize(temp);
             enemy.transform.Find("LeftFish").gameObject.GetComponent<Enemy>().Initialize(temp);
+            if (delay > 1)
+                delay -= 0.3f;
         }
 
     }
