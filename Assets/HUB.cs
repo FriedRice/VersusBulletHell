064517c832327aef3c0bb3eb2f1a4475da86b8cc
Stali <sstalev@umich.edu>
2016-03-14 21:15:36 -0400

    hubstart

---
+++ Assets/HUB.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUB : MonoBehaviour {
+
+    public static HUB S;
+
+    public GameObject[] hearts1;
+    public GameObject[] hearts2;
+
+    public void UpdateLives()
+    {
+    }
+
+    void Awake()
+    {
+        S = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
