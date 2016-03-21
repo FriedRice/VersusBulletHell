8a97851ea984513f204f3e6a13b7894cfd2dd1b1
Stali <sstalev@umich.edu>
2016-03-21 19:39:41 -0400

    made hud better

---
+++ Assets/PlayerFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollow : MonoBehaviour {
+    public bool FollowsFish = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (FollowsFish)
+        {
+            transform.position = Camera.allCameras[0].WorldToScreenPoint(Player.players[0].transform.position);
+        } else
+        {
+
+            transform.position = Camera.allCameras[1].WorldToScreenPoint(Player.players[1].transform.position);
+        }
+	}
+}
