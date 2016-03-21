8a62fb9c3677bee38f3315a34df4742afa202ea9
FriedRice <n.mclaughlin21@gmail.com>
2016-03-21 18:46:02 -0400

    Colored ally bullets.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,10 +10,14 @@ public class Enemy : MonoBehaviour {
     const string BULLET_LAYER_SUFFIX = "AllyBullet";
     const string ENEMY_BULLET_SUFFIX = "PlayerBullet";
 
+    Color FISH_BULLET_COLOR = Color.red;
+    Color BEAR_BULLET_COLOR = Color.white;
+
     public float health;
     public float time;
     public Vector3 spawn_position;
     public float side = 0; //-1=left,0=both,1 = right
+    protected Color bullet_color;
     int bullet_layer;
     string bullet_tag;
     string enemy_bullet_tag;
@@ -27,9 +31,11 @@ public class Enemy : MonoBehaviour {
             transform.Rotate(Vector3.forward, 180);
             layer_prefix = FISH_LAYER_PREFIX;
             enemy_bullet_tag = BEAR_LAYER_PREFIX + ENEMY_BULLET_SUFFIX;
+            bullet_color = FISH_BULLET_COLOR;
         } else {
             layer_prefix = BEAR_LAYER_PREFIX;
             enemy_bullet_tag = FISH_LAYER_PREFIX + ENEMY_BULLET_SUFFIX;
+            bullet_color = BEAR_BULLET_COLOR;
         }
 
         gameObject.tag = layer_prefix + ALLY_LAYER_SUFFIX;
@@ -41,6 +47,7 @@ public class Enemy : MonoBehaviour {
     protected void setBulletLayerAndTag(GameObject bullet) {
         bullet.layer = bullet_layer;
         bullet.tag = bullet_tag;
+        bullet.GetComponent<SpriteRenderer>().color = bullet_color;
     }
 
     protected bool isEnemyBulletTag(string tag) {
