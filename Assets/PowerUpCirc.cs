2982e597b25406cd5dee334ef66f9a52da4d03a2
FriedRice <n.mclaughlin21@gmail.com>
2016-03-22 00:14:19 -0400

    Made powerup ring only appear on player side.

--- Assets/PowerUpCirc.cs
+++ Assets/PowerUpCirc.cs
@@ -5,57 +5,47 @@ public class PowerUpCirc : MonoBehaviour {
     public Sprite s0, s25, s50, s75, s100;
     public bool isFish = false;
     SpriteRenderer sr;
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start() {
         sr = GetComponent<SpriteRenderer>();
-	}
+        if (isFish) {
+            gameObject.layer = LayerMask.NameToLayer("OnlyFishSide");
+        } else {
+            gameObject.layer = LayerMask.NameToLayer("OnlyBearSide");
+        }
+    }
 
     // Update is called once per frame
     void Update() {
-        if (isFish)
-        {
+        if (isFish) {
             transform.position = Player.players[0].gameObject.transform.position;
             float percentage = Player.players[0].powerup_points / (float)Player.players[0].POWERUPTHRESHOLD;
-                if (percentage < 0.25f)
-            {
+            if (percentage < 0.25f) {
                 sr.sprite = s0;
-            } else if (percentage < 0.5f)
-            {
+            } else if (percentage < 0.5f) {
                 sr.sprite = s25;
-            } else if (percentage < 0.75f)
-            {
+            } else if (percentage < 0.75f) {
                 sr.sprite = s50;
-            } else if (percentage < 1f)
-            {
+            } else if (percentage < 1f) {
                 sr.sprite = s75;
-            } else
-            {
+            } else {
                 sr.sprite = s100;
             }
-        } else
-        {
+        } else {
             transform.position = Player.players[1].gameObject.transform.position;
             float percentage = Player.players[1].powerup_points / (float)Player.players[1].POWERUPTHRESHOLD;
-            if (percentage < 0.25f)
-            {
+            if (percentage < 0.25f) {
                 sr.sprite = s0;
-            }
-            else if (percentage < 0.5f)
-            {
+            } else if (percentage < 0.5f) {
                 sr.sprite = s25;
-            }
-            else if (percentage < 0.75f)
-            {
+            } else if (percentage < 0.75f) {
                 sr.sprite = s50;
-            }
-            else if (percentage < 1f)
-            {
+            } else if (percentage < 1f) {
                 sr.sprite = s75;
-            }
-            else
-            {
+            } else {
                 sr.sprite = s100;
             }
         }
-	}
+    }
 }
